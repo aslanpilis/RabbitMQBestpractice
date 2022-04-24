96a46c17a8719fe376242e917a5b0331b0907131
QCoder <=>
2022-04-24 13:49:19 +0300

    first commit

--- RabbitMQ/Controllers/HomeController.cs
+++ RabbitMQ/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Models;
+using RabbitMQ.Watermark.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,11 +15,51 @@ namespace RabbitMQ.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly RabbitMQPublisher _rabbitMQPublisher;
+        public HomeController(ILogger<HomeController> logger, RabbitMQPublisher rabbitMQPublisher)
         {
             _logger = logger;
+            _rabbitMQPublisher = rabbitMQPublisher;
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ImageName")] Product product, IFormFile ImageFile)
+        {
+
+            if (!ModelState.IsValid) return View(product);
+
+
+            if (ImageFile.Length>0 )
+            {
+                var randomImageName = Guid.NewGuid() + Path.GetExtension(ImageFile.FileName);
+
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", randomImageName);
+
+
+                await using FileStream stream = new FileStream(path, FileMode.Create);
+
+
+                await ImageFile.CopyToAsync(stream);
+
+
+                _rabbitMQPublisher.Publish(new productImageCreatedEvent() { ImageName = randomImageName });
+
+                product.ImageName = randomImageName;
+            }
+
+
+
+
+            return RedirectToAction(nameof(Index));
+
+            return View(product);
+        }
+
 
         public IActionResult Index()
         {
--- RabbitMQ/Models/Product.cs
+++ RabbitMQ/Models/Product.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RabbitMQ.Watermark.Models
+namespace RabbitMQ.Models
 {
     public class Product
     {
